98a6fbfb8f9d992d72e17bf7c0e71c08b3ea776d
Guilherme vinicius <gvcc@cin.ufpe.br>
2021-06-14 16:16:59 -0300

    feature(props_Descrição_Carrossel):create the props for descrition carousel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import {
   PostList,
+  DescricaoCarrossel,
 } from './components';
 
 function App() {
   return (
-    <PostList />
+    <>
+     <PostList />
+     <DescricaoCarrossel text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Gravida amet in neque pretium, ut id vestibulum tempus sagittis." nome="Nome da pessoa" linkImage ="https://image.pngaaa.com/488/1418488-middle.png" /> 
+    </>
   );
 }
 
