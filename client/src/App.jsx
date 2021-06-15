17b0b8f935b0147344c6f27fdb6dd3c526ebe969
Guilherme vinicius <gvcc@cin.ufpe.br>
2021-06-15 01:37:47 -0300

    feature(section-cards-components):fix the app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
      <PostList />
-     <DescricaoCarrossel text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Gravida amet in neque pretium, ut id vestibulum tempus sagittis." nome="Nome da pessoa" linkImage ="https://image.pngaaa.com/488/1418488-middle.png" /> 
+     <DescricaoCarrossel /> 
     </>
   );
 }
